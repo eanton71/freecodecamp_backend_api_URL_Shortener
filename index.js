a771dd1d40517ad3de49719e1bf736da6f59d195
Enrique Antón López <eanton71@gmail.com>
2024-05-18 16:42:38 +0000

    0

--- index.js
+++ index.js
@@ -2,57 +2,63 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
+const bodyParser = require('body-parser');
 //cliente Redis
 const redis = require('redis');
 // Crea una instancia del cliente Redis
-const client = redis.createClient();
-
+const client = redis.createClient({
+  host: 'redis-19831.c226.eu-west-1-3.ec2.redns.redis-cloud.com',
+  port: 19831,
+  password: 'TEST_test1'
+});
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 // Genera un ID único utilizando la biblioteca `shortid`
 function generateUniqueId() {
-  const shortid = require('shortid');
-  return shortid.generate();
+  //const nanoid = require('nanoid');
+  return nanoid();
 }
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-app.get('/api/shorturl', function(req, res) {
-/*
-    const url = req.body.url;
+app.post('/api/shorturl',  (req, res) => {
+console.log(req.body);
+  const url = req.body.url;
 
-     // Genera un ID único para la URL
-     const id = generateUniqueId();
+  // Genera un ID único para la URL
+  const id = generateUniqueId();
 
-     // Guarda la URL en Redis utilizando el ID como clave
-     client.set(id, url, (err, reply) => {
-       if (err) {
-         console.error('Error al guardar la URL', err);
-         return res.status(500).json({ error: 'Error al guardar la URL' });
-       }
+  // Guarda la URL en Redis utilizando el ID como clave
+  client.set(id, url, (err, reply) => {
+    if (err) {
+      console.error('Error al guardar la URL', err);
+      return res.status(500).json({ error: 'Error al guardar la URL' });
+    }
 
-       // Devuelve la URL generada que incluye el ID
-       const generatedUrl = `https://your-backend-api-url/${id}`;
-       res.json({ generatedUrl });
-     });
-*/
- 
-  res.json({ 
-    original_url: 'hello API' ,
-  short_url:''
+    // Devuelve la URL generada que incluye el ID
+    var generatedUrl = `https://your-backend-api-url/${id}`;
+    res.json({
+      original_url: req.body.url,
+      short_url: generatedUrl
+    });
   });
+
+
+
 });
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
