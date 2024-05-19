99d088833e33ac17421b3f45ba8cf56c72a7401c
Enrique Antón López <eanton71@gmail.com>
2024-05-19 21:02:54 +0000

    0

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ import 'dotenv/config';
 import express from 'express'
 import cors from 'cors';
 const app = express();
-import  bodyParser  from 'body-parser';
+import bodyParser from 'body-parser';
 //cliente Redis
 import { createClient } from 'redis';
 // Crea una instancia del cliente Redis
 const client = await createClient({
   password: 'TEST_test1',
+  username:'url',
   socket: {
     host: 'redis-19831.c226.eu-west-1-3.ec2.redns.redis-cloud.com',
     port: 19831
   },
-  
+
   legacyMode: true
-});
+})
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
 //client.connect().catch(console.error)
 //client.connect();
 // Basic Configuration
@@ -23,7 +26,7 @@ const port = process.env.PORT || 3000;
 
 // Genera un ID único utilizando la biblioteca `shortid`
 function generateUniqueId() {
-  //const nanoid = require('nanoid');
+  //falta comprobar que no existe en la base de datos
   const id = Math.floor(Math.random() * 1000);
   console.log(id);
   return id;
@@ -58,10 +61,10 @@ app.post('/api/shorturl', (req, res) => {
     }
 
     // Devuelve la URL generada que incluye el ID
-    var generatedUrl = `https://your-backend-api-url/${id}`;
+    //var generatedUrl = `https://your-backend-api-url/${id}`;
     res.json({
       original_url: req.body.url,
-      short_url: generatedUrl
+      short_url: id
     });
   });
 
