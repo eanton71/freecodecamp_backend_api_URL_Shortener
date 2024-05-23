3cea41fdbb015b4893e8d39e161c6cb7ea7b8ca6
Enrique Antón López <eanton71@gmail.com>
2024-05-23 20:43:31 +0000

    0

--- index.js
+++ index.js
@@ -22,15 +22,6 @@ const client = await createClient({
   .connect();
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
-// Genera un ID único utilizando la biblioteca `shortid`
-function generateUniqueId() {
-  //falta comprobar que no existe en la base de datos
-  const id = Math.floor(Math.random() * 1000);
-  console.log(id);
-  return id;
-}
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -66,25 +57,19 @@ app.get('/api/shorturldb', async (req, res) => {
   res.json({});
 });*/
 app.get('/api/shorturl/:id', async (req, res) => {
-  console.log("req: " + req.params.id);
-  ///const value = await client.get(req.params.id.toString());
-  //console.log("url: " + value);
-  
 
   getValueByKey(req.params.id, (err, value) => {
     if (err) {
       console.error('Error al obtener el valor', err);
     } else {
       res.redirect(value);
-      //res.json({ url: req.params.id, value: " " + value });
     }
   });
 
 
 });
 app.post('/api/shorturl', (req, res) => {
-
-  let urlRegex = /https:\/\/www.|https:\/\/|http:\/\/www.|http:\/\/|\//g;
+  let urlRegex = /https:\/\/www.|https:\/\/|http:\/\/www.|http:\/\/|\/ /g;
   console.log(req.body.url.replace(urlRegex, ""));
   dns.lookup(req.body.url.replace(urlRegex, ""), async (err, url_Ip) => {
     if (err) {
@@ -94,20 +79,26 @@ app.post('/api/shorturl', (req, res) => {
     }
     else {
       const url = req.body.url;
-
       // Genera un ID único para la URL
-      const id = generateUniqueId();
-
-      // Guarda la URL en Redis utilizando el ID como clave
-      await client.set(id, url, (err, reply) => {
+      //var id;
+      // Uso de ejemplo
+      generateUniqueId((err, id) => {
         if (err) {
-          console.error('Error al guardar la URL', err);
-          return res.status(500).json({ error: 'Error al guardar la URL' });
+          console.error('Error al generar la clave única', err);
+        } else {
+          console.log('Clave única generada:', id);
+          // Guarda la URL en Redis utilizando el ID como clave
+          client.set(id, url, (err, reply) => {
+            if (err) {
+              console.error('Error al guardar la URL', err);
+              return res.status(500).json({ error: 'Error al guardar la URL' });
+            }
+            return res.json({
+              original_url: req.body.url,
+              short_url: id
+            });
+          });
         }
-        res.json({
-          original_url: req.body.url,
-          short_url: id
-        });
       });
     }
 
@@ -119,6 +110,30 @@ app.post('/api/shorturl', (req, res) => {
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+
+// Genera un ID único utilizando la biblioteca `shortid`
+function generateUniqueId(callback) {
+  const id = Math.floor(Math.random() * 10000);
+
+  // Verificar si la clave ya existe en la base de datos
+  client.exists(id, (err, reply) => {
+    if (err) {
+      console.error('Error al verificar la clave', err);
+      callback(err, null);
+    } else {
+      if (reply === 1) {
+        // La clave ya existe, generar una nueva clave
+        generateUniqueId(callback);
+      } else {
+        // La clave no existe, devolverla
+        callback(null, id);
+      }
+    }
+  });
+}
+
+
 // Obtener el valor de una clave
 function getValueByKey(key, callback) {
   client.get(key, (err, reply) => {
@@ -140,12 +155,12 @@ function getAllKeysAndValues(callback) {
         console.error('Error scanning keys', err);
         callback(err, null);
         return;
-      } 
+      }
       cursor = reply[0];
-      const batchKeys = reply[1]; 
+      const batchKeys = reply[1];
       if (batchKeys.length > 0) {
         keys.push(...batchKeys);
-      } 
+      }
       if (cursor === '0') {
         // All keys have been scanned, retrieve values
         getValues(keys, callback);
@@ -154,28 +169,28 @@ function getAllKeysAndValues(callback) {
         scanNextBatch();
       }
     });
-  } 
+  }
   scanNextBatch();
 }
 // Retrieve values for the keys
 function getValues(keys, callback) {
-  const values = {}; 
+  const values = {};
   keys.forEach((key, index) => {
     client.type(key, (err, reply) => {
       if (err) {
         console.error('Error retrieving data type for key', key, err);
         callback(err, null);
         return;
-      } 
-      const dataType = reply; 
+      }
+      const dataType = reply;
       if (dataType === 'hash') {
         client.hgetall(key, (err, reply) => {
           if (err) {
             console.error('Error retrieving Hash data', key, err);
             callback(err, null);
             return;
-          } 
-          values[key] = reply; 
+          }
+          values[key] = reply;
           if (index === keys.length - 1) {
             // All values have been retrieved
             callback(null, values);
@@ -187,8 +202,8 @@ function getValues(keys, callback) {
             console.error('Error retrieving String data', key, err);
             callback(err, null);
             return;
-          } 
-          values[key] = reply; 
+          }
+          values[key] = reply;
           if (index === keys.length - 1) {
             // All values have been retrieved
             callback(null, values);
@@ -203,6 +218,51 @@ function getValues(keys, callback) {
     });
   });
 }
+// Verificar si una clave existe
+function checkKeyExists(key, callback) {
+  client.exists(key, (err, reply) => {
+    if (err) {
+      console.error('Error al verificar la clave', err);
+      callback(err, null);
+    } else {
+      callback(null, reply === 1);
+    }
+  });
+}
+
+// Uso de ejemplo
+const key = 'mi-clave'; // La clave que quieres verificar
+
+checkKeyExists(key, (err, exists) => {
+  if (err) {
+    console.error('Error al verificar la clave', err);
+  } else {
+    console.log('La clave existe:', exists);
+  }
+});
+// Obtener el número de claves en la base de datos
+function getKeysCount(callback) {
+  client.dbsize((err, reply) => {
+    if (err) {
+      console.error('Error al obtener el número de claves', err);
+      callback(err, null);
+    } else {
+      callback(null, reply);
+    }
+  });
+}
+
+// Uso de ejemplo
+getKeysCount((err, count) => {
+  if (err) {
+    console.error('Error al obtener el número de claves', err);
+  } else {
+    console.log('Número de claves:', count);
+  }
+});
+
+
+
 
 // Retrieve all the keys and values
 
